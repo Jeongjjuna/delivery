9c32b70a410728c15f81b77ded35f07f06c50b87
jeongjihun <wlgns2234@naver.com>
2023-11-15 13:32:23 +0900

    chore: 초기데이터 생성
    - sql.init.mode 설정추가
    - defer-datasource-initialization 설정추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into food (price, food_type, name)
+values (7000, '중식', '짜장면');
+
+insert into food (price, food_type, name)
+values (8000, '중식', '짬뽕');
+
+insert into food (price, food_type, name)
+values (9000, '한식', '비빔밥');
