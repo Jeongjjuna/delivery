f0035f53c81d05f33deec34e2a8318ef9a0cc3a7
jeongjihun <wlgns2234@naver.com>
2023-11-21 02:00:01 +0900

    test: OrderLine 도메인 단위테스트 추가
    - 생성 테스트추가
    - 상품*수량에 대한 금액 계산 테스트

---
+++ src/test/java/com/sjincho/delivery/order/domain/OrderLineTest.java
@@ -0,0 +1,32 @@
+package com.sjincho.delivery.order.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class OrderLineTest {
+
+    @DisplayName("OrderLine 도메인 생성 테스트")
+    @Test
+    void create() {
+        assertThatCode(() -> OrderLine.create(
+                1L, 8000L,
+                2L, "짜장면"))
+                .doesNotThrowAnyException();
+    }
+
+    @DisplayName("OrderLine 주문 금액 계산 테스트")
+    @Test
+    void calculatePayment() {
+        OrderLine orderLine = new OrderLine(
+                1L, 8000L,
+                2L, "짜장면");
+
+        Long payments = orderLine.calculatePayment();
+
+        assertThat(payments).isEqualTo(16000L);
+    }
+
+}
