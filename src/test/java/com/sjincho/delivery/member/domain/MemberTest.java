ea9992d85457abb6859fe33019a9c726a14d45b9
jeongjihun <wlgns2234@naver.com>
2023-11-21 01:14:06 +0900

    test: Member 도메인 단위테스트 추가
    - 생성테스트, 수정테스트 추가

---
+++ src/test/java/com/sjincho/delivery/member/domain/MemberTest.java
@@ -0,0 +1,37 @@
+package com.sjincho.delivery.member.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class MemberTest {
+
+    @DisplayName("Member 도메인 생성 테스트")
+    @Test
+    void create() {
+        assertThatCode(() -> Member.create(
+                "홍길동",
+                "email@gmail.com",
+                "password",
+                "010-1111-2222",
+                MemberRole.CUSTOMER)).doesNotThrowAnyException();
+    }
+
+    @DisplayName("Member 도메인 수정 테스트")
+    @Test
+    void update() {
+        Member member = new Member("홍길동", "email@gmail.com", "password",
+                "010-1111-2222", MemberRole.CUSTOMER);
+
+        member.update("일지매", "updated@gmail.com", "password",
+                "010-1111-2222", MemberRole.CUSTOMER);
+
+        assertAll(
+                () -> assertThat(member.getName()).isEqualTo("일지매"),
+                () -> assertThat(member.getEmail()).isEqualTo("updated@gmail.com")
+        );
+    }
+}
