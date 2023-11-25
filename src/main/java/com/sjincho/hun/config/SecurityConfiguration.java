f7d5ec8cdfa43392b1b2a11965b28de0a3c872e3
jeongjihun <wlgns2234@naver.com>
2023-11-26 07:55:26 +0900

    feat: 회원가입을 제외한 모든 요청에 대하여 인증을 거치도록 변경

--- src/main/java/com/sjincho/hun/config/SecurityConfiguration.java
+++ src/main/java/com/sjincho/hun/config/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.sjincho.hun.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -11,7 +12,9 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(final HttpSecurity http) throws Exception {
 
-        http.authorizeHttpRequests((auth -> auth.anyRequest().permitAll()));
+        http.authorizeHttpRequests(auth -> auth
+                .requestMatchers(HttpMethod.POST, "/members").permitAll()
+        );
 
         http.csrf(csrf -> csrf.disable());
 
