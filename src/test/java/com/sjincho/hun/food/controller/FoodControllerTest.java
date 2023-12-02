f802d9765b24877ea6c21e0e2f9e441e7f401cb7
jeongjihun <wlgns2234@naver.com>
2023-12-02 17:06:29 +0900

    test: 음식 조회 API 테스트 추가
    - 음식점, 손님의 조회 요청 테스트
    - 존재하지 않는 id의 조회 요청 테스트

--- src/test/java/com/sjincho/hun/food/controller/FoodControllerTest.java
+++ src/test/java/com/sjincho/hun/food/controller/FoodControllerTest.java
@@ -1,5 +1,6 @@
 package com.sjincho.hun.food.controller;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -7,6 +8,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sjincho.hun.config.CustomerMockUser;
 import com.sjincho.hun.config.OwnerMockUser;
+import com.sjincho.hun.food.domain.Food;
 import com.sjincho.hun.food.repository.FoodJpaRepository;
 import com.sjincho.hun.member.repository.MemberJpaRepository;
 import org.junit.jupiter.api.AfterEach;
@@ -31,9 +33,6 @@ class FoodControllerTest {
     private MockMvc mockMvc;
 
     @Autowired
-    private ObjectMapper objectMapper;
-
-    @Autowired
     private FoodJpaRepository foodJpaRepository;
 
     @Autowired
@@ -43,6 +42,63 @@ class FoodControllerTest {
     public void clean() {
         foodJpaRepository.deleteAll();
         memberJpaRepository.deleteAll();
+    }
+
+    @DisplayName("특정 음식 조회 테스트 : 존재하지 않는 id 음식 조회시 404 응답 반환")
+    @Test
+    @OwnerMockUser
+    void 존재하지_않는_id의_음식조회_요청() throws Exception {
+        // given
+        Food food = Food.builder()
+                .name("짜장면")
+                .foodType("중식")
+                .price(9000L)
+                .build();
+        Food saved = foodJpaRepository.save(food);
+
+        // when, then
+        mockMvc.perform(get("/foods/{foodId}", 2)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound())
+                .andDo(print());
+    }
+
+    @DisplayName("특정 음식 조회 테스트 : 음식점이 음식조회시 200 응답 반환")
+    @Test
+    @OwnerMockUser
+    void 음식점이_음식조회_요청() throws Exception {
+        // given
+        Food food = Food.builder()
+                .name("짜장면")
+                .foodType("중식")
+                .price(9000L)
+                .build();
+        Food saved = foodJpaRepository.save(food);
+
+        // when, then
+        mockMvc.perform(get("/foods/{foodId}", saved.getId())
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+
+    @DisplayName("특정 음식 조회 테스트 : 손님이 음식조회시 200 응답 반환")
+    @Test
+    @CustomerMockUser
+    void 손님이_음식조회_요청() throws Exception {
+        // given
+        Food food = Food.builder()
+                .name("짜장면")
+                .foodType("중식")
+                .price(9000L)
+                .build();
+        Food saved = foodJpaRepository.save(food);
+
+        // when, then
+        mockMvc.perform(get("/foods/{foodId}", saved.getId())
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(print());
     }
 
     @DisplayName("음식 등록 데이터 유효성 검사")
@@ -76,7 +132,7 @@ class FoodControllerTest {
         );
     }
 
-    @DisplayName("Owner가 음식 등록 요청시 201 응답 반환")
+    @DisplayName("음식 등록 테스트 : 음식점이 음식등록시 201 응답 반환")
     @Test
     @OwnerMockUser
     void 음식점이_음식_등록() throws Exception {
@@ -97,7 +153,7 @@ class FoodControllerTest {
                 .andDo(print());
     }
 
-    @DisplayName("Customer가 음식 등록 요청시 403 응답 반환")
+    @DisplayName("음식 등록 테스트 : 손님이 음식등록시 403 응답 반환")
     @Test
     @CustomerMockUser
     void 손님이_음식_등록() throws Exception {
