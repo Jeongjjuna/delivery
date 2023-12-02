34ac9a91856ece31764f9378fe5a920127f97037
jeongjihun <wlgns2234@naver.com>
2023-12-03 00:48:19 +0900

    test: 회원 가입 API 테스트 추가
    - 회원 가입 테스트
    - 중복된 이메일로 회원가입 테스트
    - 유효하지 않은 요청데이터 테스트

---
+++ src/test/java/com/sjincho/hun/member/controller/MemberControllerTest.java
@@ -0,0 +1,131 @@
+package com.sjincho.hun.member.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.sjincho.hun.member.domain.Member;
+import com.sjincho.hun.member.domain.MemberRole;
+import com.sjincho.hun.member.repository.MemberJpaRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import java.util.stream.Stream;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@DisplayName("Member 도메인 API 테스트")
+class MemberControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private MemberJpaRepository memberJpaRepository;
+
+    @AfterEach
+    public void clean() {
+        memberJpaRepository.deleteAll();
+    }
+
+    @DisplayName("회원가입 데이터 유효성 검사")
+    @ParameterizedTest(name = "name:{0}, email:{1}, password:{2}, cellPhone:{3}, memberRole:{4} 회원가입 요청시 400 응답 반환")
+    @MethodSource("provideRequestForInvalidData")
+    void 회원가입_데이터_유효성_검사(String name, String email, String password, String cellPhone, String memberRole) throws Exception {
+        // given
+        String invalidJson = """
+                {
+                  "name" : %s,
+                  "email" : %s,
+                  "password" : %s,
+                  "cellPhone" : %s,
+                  "memberRole" : %s
+                }
+                """.formatted(name, email, password, cellPhone, memberRole);
+
+        // when, then
+        mockMvc.perform(post("/members")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(invalidJson))
+                .andExpect(status().isBadRequest())
+                .andDo(print());
+    }
+
+    @DisplayName("회원 가입 테스트 : 중복된 이메일로 회원 가입시 409 응답 반환")
+    @Test
+    void 회원_가입시_중복된_이메일로_가입() throws Exception {
+        // given
+        Member member = Member.builder()
+                .name("name")
+                .email("user1@naver.com")
+                .password("password")
+                .cellPhone("000-0000-0000")
+                .memberRole(MemberRole.CUSTOMER)
+                .build();
+        memberJpaRepository.save(member);
+
+        String json = """
+                {
+                  "name" : "정지훈",
+                  "email" : "user1@naver.com",
+                  "password" : "password",
+                  "cellPhone" : "010-1111-2222",
+                  "memberRole" : "CUSTOMER"
+                }
+                """;
+
+        // when, then
+        mockMvc.perform(post("/members")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                .andExpect(status().isConflict())
+                .andDo(print());
+    }
+
+    @DisplayName("회원 가입 테스트 : 회원 가입시 201 응답 반환")
+    @Test
+    void 회원_가입() throws Exception {
+        // given
+        String json = """
+                {
+                  "name" : "정지훈",
+                  "email" : "user1@naver.com",
+                  "password" : "password",
+                  "cellPhone" : "010-1111-2222",
+                  "memberRole" : "CUSTOMER"
+                }
+                """;
+
+        // when, then
+        mockMvc.perform(post("/members")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                .andExpect(status().isCreated())
+                .andDo(print());
+    }
+
+    private static Stream<Arguments> provideRequestForInvalidData() {
+        return Stream.of(
+                Arguments.of("", "user1@gmail.com", "pw", "000-0000-0000", "CUSTOMER"),
+                Arguments.of("name", "", "pw", "000-0000-0000", "CUSTOMER"),
+                Arguments.of("name", "user1@gmail.com", "", "000-0000-0000", "CUSTOMER"),
+                Arguments.of("name", "user1@gmail.com", "pw", "", "CUSTOMER"),
+                Arguments.of("name", "user1@gmail.com", "pw", "000-0000-0000", ""),
+
+                Arguments.of(null, "user1@gmail.com", "pw", "000-0000-0000", "CUSTOMER"),
+                Arguments.of("name", null, "pw", "000-0000-0000", "CUSTOMER"),
+                Arguments.of("name", "user1@gmail.com", null, "000-0000-0000", "CUSTOMER"),
+                Arguments.of("name", "user1@gmail.com", "pw", null, "CUSTOMER"),
+                Arguments.of("name", "user1@gmail.com", "pw", "000-0000-0000", null)
+        );
+    }
+
+}
