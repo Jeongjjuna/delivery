13ecaeecf567f9ff23fe7b7ac966bea10fc461ef
jeongjihun <wlgns2234@naver.com>
2023-12-02 15:44:15 +0900

    test: 음식 등록 API 테스트 추가
    - 음식 등록 요청데이터에 유효하지 않은 값이 들어간 경우 401응답을 반환한다.

--- src/test/java/com/sjincho/hun/food/controller/FoodControllerTest.java
+++ src/test/java/com/sjincho/hun/food/controller/FoodControllerTest.java
@@ -7,16 +7,20 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sjincho.hun.config.CustomerMockUser;
 import com.sjincho.hun.config.OwnerMockUser;
-import com.sjincho.hun.food.dto.FoodCreateRequest;
 import com.sjincho.hun.food.repository.FoodJpaRepository;
+import com.sjincho.hun.member.repository.MemberJpaRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import java.util.stream.Stream;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -32,9 +36,44 @@ class FoodControllerTest {
     @Autowired
     private FoodJpaRepository foodJpaRepository;
 
+    @Autowired
+    private MemberJpaRepository memberJpaRepository;
+
     @AfterEach
     public void clean() {
         foodJpaRepository.deleteAll();
+        memberJpaRepository.deleteAll();
+    }
+
+    @DisplayName("음식 등록 데이터 유효성 검사")
+    @ParameterizedTest(name = "name:{0}, foodType:{1}, price:{2} 음식 등록 요청시 401 응답 반환")
+    @MethodSource("provideRequestForInvalidData")
+    @OwnerMockUser
+    void 음식_등록_데이터_유효성_검사(String name, String foodType, String price) throws Exception {
+        // given
+        String invalidJson = """
+                {
+                  "name" : %s,
+                  "foodType" : %s,
+                  "price" : %s
+                }
+                """.formatted(name, foodType, price);
+
+        // when, then
+        mockMvc.perform(post("/foods")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(invalidJson))
+                .andExpect(status().isBadRequest())
+                .andDo(print());
+    }
+
+    private static Stream<Arguments> provideRequestForInvalidData() {
+        return Stream.of(
+                Arguments.of(null, "한식", "10000"),
+                Arguments.of("볶음밥", null, "10000"),
+                Arguments.of("볶음밥", "한식", null),
+                Arguments.of("볶음밥", "한식", "-1")
+        );
     }
 
     @DisplayName("Owner가 음식 등록 요청시 201 응답 반환")
@@ -42,13 +81,13 @@ class FoodControllerTest {
     @OwnerMockUser
     void 음식점이_음식_등록() throws Exception {
         // given
-        FoodCreateRequest request = FoodCreateRequest.builder()
-                .name("볶음밥")
-                .foodType("한식")
-                .price(10000L)
-                .build();
-
-        String json = objectMapper.writeValueAsString(request);
+        String json = """
+                {
+                  "name" : "볶음밥",
+                  "price" : 10000,
+                  "foodType" : "한식"
+                }
+                """;
 
         // when, then
         mockMvc.perform(post("/foods")
@@ -63,13 +102,13 @@ class FoodControllerTest {
     @CustomerMockUser
     void 손님이_음식_등록() throws Exception {
         // given
-        FoodCreateRequest request = FoodCreateRequest.builder()
-                .name("볶음밥")
-                .foodType("한식")
-                .price(10000L)
-                .build();
-
-        String json = objectMapper.writeValueAsString(request);
+        String json = """
+                {
+                  "name" : "볶음밥",
+                  "price" : 10000,
+                  "foodType" : "한식"
+                }
+                """;
 
         // when, then
         mockMvc.perform(post("/foods")
