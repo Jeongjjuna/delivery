d241bf312470b816331706891bbb7813590bd58b
jeongjihun <wlgns2234@naver.com>
2023-12-24 05:26:41 +0900

    feat: database Lock기능 추가
    - 주문수락/취소 동시요청시 데이터 정합성 문제에 따라, 비관적 Lock 기능 적용

--- src/main/java/com/sjincho/hun/order/infrastructure/OrderJpaRepository.java
+++ src/main/java/com/sjincho/hun/order/infrastructure/OrderJpaRepository.java
@@ -2,9 +2,11 @@ package com.sjincho.hun.order.infrastructure;
 
 import com.sjincho.hun.order.domain.OrderStatus;
 import com.sjincho.hun.order.infrastructure.entity.OrderEntity;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import java.util.Optional;
@@ -14,6 +16,7 @@ public interface OrderJpaRepository extends JpaRepository<OrderEntity, Long> {
     @Query("SELECT o FROM orders o JOIN FETCH o.memberEntity")
     Page<OrderEntity> findAllWithMember(Pageable pageable);
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("SELECT o FROM orders o JOIN FETCH o.memberEntity WHERE o.id = :orderId")
     Optional<OrderEntity> findByIdWithMember(@Param("orderId") Long orderId);
 
